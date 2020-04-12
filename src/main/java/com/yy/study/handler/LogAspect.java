98d8c5e12e34a84486608067bc29fd16b030fc80
isntyy <yysama104@163.com>
2020-04-12 22:22:54 +0800

    aop记录操作日志

---
+++ src/main/java/com/yy/study/handler/LogAspect.java
@@ -0,0 +1,21 @@
+package com.yy.study.handler;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 日志记录
+ *
+ * @author wyy
+ * @since 2020-04-12 22:13
+ */
+@Component
+//@Aspect
+public class LogAspect {
+
+    private static  String METHOD_GET = "GET";
+
+    //@PointCut
+    public void logPoint(){
+
+    }
+}
